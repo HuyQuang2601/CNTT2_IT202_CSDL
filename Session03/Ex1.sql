6f176c20eb07e9b3fbf9f918b68fde6017413c48
huy <huyxauzai2601@gmail.com>
2025-12-31 01:07:13 +0700

    update SS4

--- Session03/Ex1.sql
+++ Session03/Ex1.sql
@@ -1,6 +1,6 @@
 USE students;
 CREATE TABLE Student (
-    student_id VARCHAR(10) PRIMARY KEY,
+    student_id INT PRIMARY KEY,
     full_name NVARCHAR(100) NOT NULL,
     date_of_birth DATE,
     email VARCHAR(100) UNIQUE
@@ -8,12 +8,14 @@ CREATE TABLE Student (
 
 INSERT INTO student (student_id, full_name, date_of_birth, email)
 VALUES 
-('SV001', 'Nguyễn Văn An', '2003-05-12', 'an.nguyen@gmail.com'),
-('SV002', 'Trần Thị Bình', '2002-11-20', 'binh.tran@gmail.com'),
-('SV003', 'Lê Hoàng Minh', '2003-08-01', 'minh.le@gmail.com');
+('1', 'Nguyễn Văn An', '2003-05-12', 'an.nguyen@gmail.com'),
+('2', 'Trần Thị Bình', '2002-11-20', 'binh.tran@gmail.com'),
+('3', 'Lê Hoàng Minh', '2003-08-01', 'minh.le@gmail.com');
+
 
 SELECT * 
 FROM Student;
 
 SELECT student_id, full_name 
 FROM Student;
+
